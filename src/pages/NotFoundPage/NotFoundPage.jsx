47c31cb52329827e4c0caa1a54d949f17cac8eee
NataliiaSwan <148957684+NataliiaSwan@users.noreply.github.com>
2024-06-19 13:05:21 +0300

    Update vercel.json

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <Link to="/">Go to Home</Link>
+    // <div>
+    //   <Link to="/">Go to Home</Link>
+    // </div>
+    <div className={css.notFoundPage}>
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link to="/">Go to Home Page</Link>
     </div>
   );
 };
